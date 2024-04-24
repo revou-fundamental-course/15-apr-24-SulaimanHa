a0755279a65a23ecce95273aece093ef11981e53
SulaimanHa <sulaiman.hasan33@gmail.com>
2024-04-24 21:22:02 +0700

    Convert selesai

--- js/script.js
+++ js/script.js
@@ -20,10 +20,26 @@ function reverse() {
     }
 }
 
-function convert() {
+function convertC() {
     let angkaCel = document.getElementById("angkaCel").value;
     if(isNaN(angkaCel)){
         alert("Tolong Input Angka saja");
     }
     console.log(angkaCel);
+
+    let hitungF = (angkaCel*9/5)+32;
+
+    document.getElementById("hasilFah").value=hitungF;
+}
+
+function convertF() {
+    let angkaFah = document.getElementById("angkaFah").value;
+    if(isNaN(angkaFah)){
+        alert("Tolong Input Angka saja");
+    }
+    console.log(angkaFah);
+
+    let hitungC = (angkaFah-32)*5/9;
+
+    document.getElementById("hasilCel").value=hitungC;
 }
