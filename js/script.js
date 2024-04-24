245841a1e142ce00baa9f2f37273a25c4f134673
SulaimanHa <sulaiman.hasan33@gmail.com>
2024-04-24 20:55:37 +0700

    reverse system

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 // ini File JS
+
+let isReverse = true;
+
+function reverse() {
+
+    let CtoF = document.getElementById('CtoF');
+    let FtoC = document.getElementById('FtoC');
+
+    if (isReverse) {
+        console.log(isReverse);
+        FtoC.style.display = "block";
+        CtoF.style.display = "none";
+        isReverse = false;
+    } else {
+        console.log(isReverse);
+        FtoC.style.display = "none";
+        CtoF.style.display = "block";
+        isReverse = true;
+    }
+}
+
+function convert() {
+    let angkaCel = document.getElementById("angkaCel").value;
+    if(isNaN(angkaCel)){
+        alert("Tolong Input Angka saja");
+    }
+    console.log(angkaCel);
+}
